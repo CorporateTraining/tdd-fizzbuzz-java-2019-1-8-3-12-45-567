8be5388f66a97f86cc72885313ba31c910bed117
mahuanfy <mahuanfy@foxmail.com>
2019-04-20 18:53:49 +0800

    Add two conditions test.

--- src/test/java/com/thoughtworks/tdd/GameTest.java
+++ src/test/java/com/thoughtworks/tdd/GameTest.java
@@ -8,21 +8,21 @@ import org.junit.Test;
 public class GameTest {
 
     @Test
-    public void input_3_out_put_Fizz(){
+    public void input_3_multiple_out_put_Fizz(){
         Game game = new Game();
         String result = game.IsThreeMultiple(6);
         Assert.assertEquals(result, "Fizz");
     }
 
     @Test
-    public void input_5_out_put_Buzz(){
+    public void input_5_multiple_out_put_Buzz(){
         Game game = new Game();
         String result = game.IsThreeMultiple(10);
         Assert.assertEquals(result, "Buzz");
     }
 
     @Test
-    public void input_7_out_put_Whizz(){
+    public void input_7_multiple_out_put_Whizz(){
         Game game = new Game();
         String result = game.IsThreeMultiple(14);
         Assert.assertEquals(result, "Whizz");
@@ -34,5 +34,26 @@ public class GameTest {
         String result = game.IsThreeMultiple(11);
         Assert.assertEquals(result, "11");
     }
+
+    @Test
+    public void input_3_and_5_out_put_Whizz(){
+        Game game = new Game();
+        String result = game.IsThreeMultiple(11);
+        Assert.assertEquals(result, "FizzBuzz");
+    }
+    @Test
+    public void input_5_and_7_out_put_Whizz(){
+        Game game = new Game();
+        String result = game.IsThreeMultiple(11);
+        Assert.assertEquals(result, "BuzzWhizz");
+    }
+    @Test
+    public void input_3_and_7_out_put_Whizz(){
+        Game game = new Game();
+        String result = game.IsThreeMultiple(11);
+        Assert.assertEquals(result, "FizzWhizz");
+    }
+
+
 
 }
